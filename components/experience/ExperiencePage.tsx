827a80f9eb68e5b4e8d3ffd45c20a9ee28509181
sumitsingh4411 <shiv6371913458@gmail.com>
2022-10-30 11:03:55 +0530

    changing home page image

--- components/experience/ExperiencePage.tsx
+++ components/experience/ExperiencePage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { EXPERIENCE } from "./constant";
 import style from "./Experience.module.scss";
 
 export default function ExperiencePage() {
@@ -6,69 +7,47 @@ export default function ExperiencePage() {
     <div className={style.experiencePage}>
       <h1>Experience</h1>
       <div className={style.experience}>
-        <div className={style.experience__item}>
-          <h2>Frontend Developer</h2>
-          <h3>ByteLearn</h3>
-          <h4>2021 - Present</h4>
-          <ul className={style.experience__item__list}>
-            <li>
-              Developed a web application for a client using React and
-              TypeScript
-            </li>
-            <li>
-              Developed a web application for a client using React and
-              TypeScript
-            </li>
-            <li>
-              Developed a web application for a client using React and
-              TypeScript
-            </li>
-          </ul>
-          <ul className={style.experience__skills}>
-            <li>React</li>
-            <li>Next.js</li>
-            <li>JavaScript</li>
-            <li>TypeScript</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>SCSS</li>
-            <li>Git</li>
-            <li>GitHub</li>
-            <li>VS Code</li>
-          </ul>
-        </div>
-
-        <div className={style.experience__item}>
-          <h2>Frontend Developer</h2>
-          <h3>ByteLearn</h3>
-          <h4>2021 - Present</h4>
-          <ul className={style.experience__item__list}>
-            <li>
-              Developed a web application for a client using React and
-              TypeScript
-            </li>
-            <li>
-              Developed a web application for a client using React and
-              TypeScript
-            </li>
-            <li>
-              Developed a web application for a client using React and
-              TypeScript
-            </li>
-          </ul>
-          <ul className={style.experience__skills}>
-            <li>React</li>
-            <li>Next.js</li>
-            <li>JavaScript</li>
-            <li>TypeScript</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>SCSS</li>
-            <li>Git</li>
-            <li>GitHub</li>
-            <li>VS Code</li>
-          </ul>
-        </div>
+        {EXPERIENCE.map((experience: any, index: number) => (
+          <div className={style.experienceItem} key={index}>
+            <div
+              className={style.experienceItem_line}
+              style={{
+                height: 399,
+              }}
+            ></div>
+            <div className={style.experience__item}>
+              <h2>Frontend Developer</h2>
+              <h3>ByteLearn</h3>
+              <h4>2021 - Present</h4>
+              <ul className={style.experience__item__list}>
+                <li>
+                  Developed a web application for a client using React and
+                  TypeScript
+                </li>
+                <li>
+                  Developed a web application for a client using React and
+                  TypeScript
+                </li>
+                <li>
+                  Developed a web application for a client using React and
+                  TypeScript
+                </li>
+              </ul>
+              <ul className={style.experience__skills}>
+                <li>React</li>
+                <li>Next.js</li>
+                <li>JavaScript</li>
+                <li>TypeScript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>SCSS</li>
+                <li>Git</li>
+                <li>GitHub</li>
+                <li>VS Code</li>
+              </ul>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ components/experience/constant.ts
@@ -0,0 +1,59 @@
+export const EXPERIENCE = [
+  {
+    name: "Frontend Developer",
+    company: "Softrams",
+    date: "2020 - Present",
+    description: [
+      "Developing a web application for a client in the healthcare industry using React, Redux, TypeScript, and Next.js.",
+      "Developing a web application for a client in the healthcare industry using React, Redux, TypeScript, and Next.js.",
+      "Developing a web application for a client in the healthcare industry using React, Redux, TypeScript, and Next.js.",
+    ],
+    technologies: [
+      "React",
+      "Redux",
+      "TypeScript",
+      "Next.js",
+      "React",
+      "Redux",
+      "TypeScript",
+    ],
+  },
+  {
+    name: "Frontend Developer",
+    company: "Softrams",
+    date: "2020 - Present",
+    description: [
+      "Developing a web application for a client in the healthcare industry using React, Redux, TypeScript, and Next.js.",
+      "Developing a web application for a client in the healthcare industry using React, Redux, TypeScript, and Next.js.",
+      "Developing a web application for a client in the healthcare industry using React, Redux, TypeScript, and Next.js.",
+    ],
+    technologies: [
+      "React",
+      "Redux",
+      "TypeScript",
+      "Next.js",
+      "React",
+      "Redux",
+      "TypeScript",
+    ],
+  },
+  {
+    name: "Frontend Developer",
+    company: "Softrams",
+    date: "2020 - Present",
+    description: [
+      "Developing a web application for a client in the healthcare industry using React, Redux, TypeScript, and Next.js.",
+      "Developing a web application for a client in the healthcare industry using React, Redux, TypeScript, and Next.js.",
+      "Developing a web application for a client in the healthcare industry using React, Redux, TypeScript, and Next.js.",
+    ],
+    technologies: [
+      "React",
+      "Redux",
+      "TypeScript",
+      "Next.js",
+      "React",
+      "Redux",
+      "TypeScript",
+    ],
+  },
+];

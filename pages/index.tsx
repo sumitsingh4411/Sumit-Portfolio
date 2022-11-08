9a29fe644de28a326731b4e18c865eed915bc467
Admin <admin@Admins-MacBook-Pro.local>
2022-11-08 07:35:49 +0530

    change hoem page style page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import ContactPage from "../components/contact/ContactPage";
-import CVPage from "../components/cv/CVPage";
-import ExperiencePage from "../components/experience/ExperiencePage";
 import HomePage from "../components/home/HomePage";
-import ProjectPage from "../components/projects/ProjectPage";
-import SkillsPage from "../components/skills/SkillsPage";
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>The sumit</title>
+        <title>about sumit</title>
         <meta name="description" content="sumit singh portfolio" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="./../assets/sumit.jpg" />
       </Head>
       <HomePage />
     </>

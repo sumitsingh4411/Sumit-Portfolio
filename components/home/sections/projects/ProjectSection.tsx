1c3271a6ad8d24527774500c35bc2e25383d78cc
sumitsingh4411 <shiv6371913458@gmail.com>
2022-11-16 10:06:45 +0530

    update projects page and link with github

--- components/home/sections/projects/ProjectSection.tsx
+++ components/home/sections/projects/ProjectSection.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import React from "react";
 import { PROJECTS } from "./constant";
 import style from "./ProjectSection.module.scss";
+import github from "../../../../assets/github.svg";
+import live from "../../../../assets/external.svg";
 
 export default function ProjectSection() {
   return (
@@ -27,21 +29,45 @@ export default function ProjectSection() {
                   alt="project"
                   width={600}
                   height={350}
+                  style={{ borderRadius: "4px" }}
                 />
+                <div
+                  className={
+                    style.projectSection_container_body__image__overlay
+                  }
+                ></div>
               </div>
               <div
                 className={style.projectSection_container__body__project__right}
               >
                 <p className={style.project_featured}>Featured Project</p>
-                <h1 className={style.project_title}>{project.title}</h1>
+                <h2 className={style.project_title}>{project.title}</h2>
                 <div className={style.project_description}>
                   <p>{project?.description}</p>
                 </div>
-                <p>
+                <p className={style.project_teachnology_used}>
                   {project?.technologies?.map((technology, index) => (
                     <span key={index}>{technology}</span>
                   ))}
                 </p>
+                <div className={style.project_links}>
+                  <Image
+                    src={github}
+                    alt="github"
+                    width={24}
+                    height={24}
+                    className={style.project_link}
+                    onClick={() => window.open(project.github_url)}
+                  />
+                  <Image
+                    src={live}
+                    alt="live"
+                    width={22}
+                    height={22}
+                    className={style.project_link}
+                    onClick={() => window.open(project.live_url)}
+                  />
+                </div>
               </div>
             </div>
           ))}
--- components/home/sections/projects/constant.tsx
+++ components/home/sections/projects/constant.tsx
@@ -1,12 +1,49 @@
 export const PROJECTS = [
   {
     id: 1,
-    title: "E-commerce",
+    title: "Chose Your Resort",
     description:
-      "E-commerce website with React, Redux, Firebase, Stripe, and Sass.",
-    image_url: "https://user-images.githubusercontent.com/76992713/200154557-a40b8514-feed-4395-8d5b-7832ab2c5855.png",
-    github_url: "",
-    live_url: "",
-    technologies: ["React", "Redux", "Firebase", "Stripe", "Sass"],
+      "A website that allows users to choose a resort and book a room. It also allows users to view the resort's amenities and facilities. With this website, users can see the proper resort for them.",
+    image_url:
+      "https://camo.githubusercontent.com/7c9a2c6f239fe06d2f578533c9d57bcea14b439ba1bbe697a2fd0b34eaa822ff/68747470733a2f2f692e6962622e636f2f44476e54386b4b2f43617074757265352e706e67",
+    github_url: "https://github.com/sumitsingh4411/Choose-Your-Resort",
+    live_url: "https://beachresortz.herokuapp.com/",
+    technologies: ["React", "Redux", "JavaScript", "CSS"],
+  },
+  {
+    id: 2,
+    title: "Daily Life Expense Tracker",
+    description:
+      "A website that allows users to track their daily expenses. It also allows users to view their expenses in a graphical format. With this website, users can see their expenses and can plan their budget accordingly.",
+    image_url:
+      "https://camo.githubusercontent.com/1a7bf490d240850b807197ef70320bcd129e924186469087bbb86fa89196f9a0/68747470733a2f2f692e6962622e636f2f5a7868464e70362f436170747572652e706e67",
+    github_url: "https://github.com/sumitsingh4411/Daily-Life-Expense-Tracker",
+    live_url: "https://expensetrackerz.herokuapp.com/",
+    technologies: [
+      "React",
+      "Redux",
+      "JavaScript",
+      "CSS",
+      "Chart.js",
+      "Speechly",
+    ],
+  },
+  {
+    id: 3,
+    title: "YourCart (Ecommerce application)",
+    description:
+      "A website that allows users to buy products related with plants. It also allows users to add products to their cart and checkout. With this website, users can buy products and can also see their orders.",
+    image_url:
+      "https://camo.githubusercontent.com/759078a3bbe85f7a6d6542509ecc770f8fd6da3c82e281d8e964f80974ae7be0/68747470733a2f2f692e6962622e636f2f505142534a63762f436170747572652e706e67",
+    github_url: "https://github.com/sumitsingh4411/Your-Cart-Ecommerce-App-",
+    live_url: "https://yourcartz.herokuapp.com/",
+    technologies: [
+      "React",
+      "Redux",
+      "JavaScript",
+      "CSS",
+      "Material UI",
+      "Commerce.js",
+    ],
   },
 ];

22db2c35672eb518e4a9da9a854a48e0bcda691b
sumitsingh4411 <shiv6371913458@gmail.com>
2022-11-16 14:40:30 +0530

    update projects page and link with github

--- common/helpers.ts
+++
@@ -1,9 +0,0 @@
-import { PROJECTS_URLS } from "../components/projects/constant";
-
-export function getProjectUrl(id: any) {
-  let temp_data = PROJECTS_URLS?.filter((item) => item?.id === id);
-  if (temp_data) {
-    return temp_data[0]?.url;
-  }
-  return [];
-}

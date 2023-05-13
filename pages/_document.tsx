dafb9ebd64ad2b0ff5e2b23f46a907d4740ad7eb
Admin <admin@Admins-MacBook-Pro.local>
2023-05-13 19:37:57 +0530

    addin hotjar for tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
             __html: `
                 (function(h,o,t,j,a,r){
                   h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                  h._hjSettings={hjid:3490458,hjsv:6};
+                  h._hjSettings={hjid:3490533,hjsv:6};
                   a=o.getElementsByTagName('head')[0];
                   r=o.createElement('script');r.async=1;
                   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
